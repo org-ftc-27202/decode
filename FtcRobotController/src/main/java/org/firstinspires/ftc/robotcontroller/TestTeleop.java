474427dc64bb884ac9c4d8c5417bbb1dc201a788
fishs <fishstripe1@gmail.com>
2025-10-08 20:17:52 -0500

    thomas work on teleop, should work

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/TestTeleop.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/TestTeleop.java
@@ -1,28 +1,24 @@
 package org.firstinspires.ftc.robotcontroller;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import dev.nextftc.bindings.BindingManager;
 import dev.nextftc.bindings.Button;
 import dev.nextftc.core.components.BindingsComponent;
-import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.extensions.pedro.FollowPath;
 import dev.nextftc.extensions.pedro.PedroComponent;
-import dev.nextftc.extensions.pedro.PedroDriverControlled;
 import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
-import dev.nextftc.hardware.driving.DriverControlledCommand;
 import dev.nextftc.hardware.driving.MecanumDriverControlled;
 import dev.nextftc.hardware.impl.MotorEx;
 import static dev.nextftc.bindings.Bindings.*;
-
 import static dev.nextftc.extensions.pedro.PedroComponent.follower;
 
-
 @TeleOp(name = "NextFTC TeleOp Program Java")
-public class JetEngine extends NextFTCOpMode {
-    public JetEngine() {
+public class TestTeleop extends NextFTCOpMode {
+    public TestTeleop() {
         addComponents(
                 BulkReadComponent.INSTANCE,
                 BindingsComponent.INSTANCE,
@@ -33,43 +29,40 @@ public class JetEngine extends NextFTCOpMode {
     private MotorEx frontRightMotor = new MotorEx("rf").reversed();
     private MotorEx backLeftMotor = new MotorEx("lr").reversed();
     private MotorEx backRightMotor = new MotorEx("rr").reversed();
-
+    private final Pose startPose = new Pose(5, 72, Math.toRadians(0));
+    private final Pose basePose = new Pose(24, 120 , Math.toRadians(0));
+    Path BaseMovePath;
+    FollowPath baseMove;
+    public void buildPaths() {
+        BaseMovePath = new Path(new BezierLine(startPose, basePose));
+        BaseMovePath.setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(90), .8);
+    }
     MecanumDriverControlled drivercontrolled = new MecanumDriverControlled(
             frontLeftMotor,
             frontRightMotor,
             backLeftMotor,
             backRightMotor,
             Gamepads.gamepad1().leftStickY(),
-    Gamepads.gamepad1().leftStickX().negate(),
-    Gamepads.gamepad1().rightStickX());
+            Gamepads.gamepad1().leftStickX().negate(),
+            Gamepads.gamepad1().rightStickX());
     Button myButton = button(() -> gamepad1.a);
-
     @Override public void onInit(){
-
-
+        buildPaths();
+        baseMove = new FollowPath(BaseMovePath, true);
+        follower().setStartingPose(startPose);
     }
     @Override public void onStartButtonPressed(){
         drivercontrolled.schedule();
+        myButton.whenBecomesTrue(() -> baseMove.schedule());
     }
-
     @Override public void onUpdate() {
-        myButton.whenBecomesTrue(() -> drivercontrolled.stop(true));
-        myButton.whenBecomesFalse(() -> drivercontrolled.schedule());
         BindingManager.update();
         telemetry.addData("position", follower().getPose());
         telemetry.addData("heading", follower().getHeading());
         telemetry.update();
-
-
     }
     @Override public void onStop(){
-
         BindingManager.reset();
+
     }
-
-
-
-
-
-
 }
