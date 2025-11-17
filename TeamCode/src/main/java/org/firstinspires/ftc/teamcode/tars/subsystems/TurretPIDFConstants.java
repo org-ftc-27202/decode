2fadda1430f12b76757bdfaa3155e3cbd1418d44
ftc27 <ftc27202@gmail.com>
2025-11-16 20:05:18 -0600

    Thomas pidf

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tars/subsystems/TurretPIDFConstants.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.tars.subsystems;
+
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
+public class TurretPIDFConstants {
+    public static double p = 725;
+    public static double i = 100;
+    public static double d = 0;
+    public static double f = 0;
+}
